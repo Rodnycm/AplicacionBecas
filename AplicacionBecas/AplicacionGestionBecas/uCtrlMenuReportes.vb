9de49238042c02e38936f0e938da30068af7a952
mmullerc <mmullerc@ucenfotec.ac.cr>
2014-10-31 23:16:38 -0700

    agregar reporte de registro de acciones

--- AplicacionBecas/AplicacionGestionBecas/uCtrlMenuReportes.vb
+++ AplicacionBecas/AplicacionGestionBecas/uCtrlMenuReportes.vb
@@ -2,12 +2,12 @@
 
     Private Sub btnConsultarBitacoraAcciones_Click(sender As Object, e As EventArgs) Handles btnConsultarBitacoraAcciones.Click
 
-        Dim uCtrlBitacoraAcciones As New uCtrlReporteAcciones()
+        Dim uCtrl As New uCtrlReporteRegistroAcciones()
 
-        frmPrincipal.Controls.Add(uCtrlBitacoraAcciones)
-        uCtrlBitacoraAcciones.Show()
-        uCtrlBitacoraAcciones.BringToFront()
-        uCtrlBitacoraAcciones.Location = New Point(300, 50)
+        frmPrincipal.Controls.Add(uCtrl)
+        uCtrl.Show()
+        uCtrl.BringToFront()
+        uCtrl.Location = New Point(300, 50)
         Me.Hide()
 
     End Sub
