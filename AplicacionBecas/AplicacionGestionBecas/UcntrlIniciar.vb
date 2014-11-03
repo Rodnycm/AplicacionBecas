428b0b3ed78c2909baf6f50c609b3096246aa96f
RodnyPapi <rcastrom@ucenfotec.ac.cr>
2014-11-03 14:50:00 -0600

    Merge Mathias

--- AplicacionBecas/AplicacionGestionBecas/UcntrlIniciar.vb
+++ AplicacionBecas/AplicacionGestionBecas/UcntrlIniciar.vb
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-﻿Imports EntitiesLayer
+
+Imports EntitiesLayer
 Public Class UcntrlIniciar
 
     Dim alerta As UctrlAlerta
@@ -43,51 +43,4 @@ Public Class UcntrlIniciar
     End Sub
 
 End Class
-=======
-﻿Imports EntitiesLayer
-Public Class UcntrlIniciar
 
-    Dim alerta As UctrlAlerta = New UctrlAlerta()
-
-    Private Sub btnIniciarSesion_Click(sender As Object, e As EventArgs) Handles btnIniciarSesion.Click
-        MsgBox("Matihass mas")
-
-        Dim nombreUsuario As String = txtNombreUsuario.Text
-        Dim contraseña As String = txtContraseña.Text
-        Dim listaUsuarios As List(Of Usuario)
-
-        Try
-            listaUsuarios = objGestorUsuario.iniciarSesion(nombreUsuario, contraseña)
-
-            If listaUsuarios.Count > 0 Then
-                Me.Hide()
-                Globals.userName = listaUsuarios(0).primerNombre
-                Globals.userRol = listaUsuarios(0).rol
-                FrmIniciarSesion.Hide()
-                FrmIniciarSesion.principal.Show()
-            Else
-                alerta.lblAlerta.Text = "Nombre de usuario o contraseña incorrectos"
-                Me.Controls.Add(alerta)
-                alerta.BringToFront()
-                alerta.Location = New Point(100, 150)
-                alerta.Show()
-            End If
-        Catch ex As Exception
-            alerta.lblAlerta.Text = ex.Message
-            FrmIniciarSesion.principal.Controls.Add(alerta)
-            alerta.BringToFront()
-            alerta.Show()
-        End Try
-
-    End Sub
-
-    Private Sub btnRecuperar_Click(sender As Object, e As EventArgs) Handles btnRecuperar.Click
-
-        Dim uctrlRecuperar As UctrlRecuperarContraseña = New UctrlRecuperarContraseña()
-        FrmIniciarSesion.Controls.Add(uctrlRecuperar)
-        uctrlRecuperar.BringToFront()
-        uctrlRecuperar.Location = New Point(140, 260)
-    End Sub
-
-End Class
->>>>>>> origin/Mathias
